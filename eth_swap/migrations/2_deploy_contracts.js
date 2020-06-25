2503999b4f7092ec07f180e95c8bf8605da2a210
lopeselio <lopeselio@gmail.com>
2020-06-25 19:14:35 +0530

    Project steup

--- eth_swap/migrations/2_deploy_contracts.js
+++ eth_swap/migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
 const EthSwap = artifacts.require("EthSwap")
+const Token = artifacts.require("Token")
 
-module.exports = function(deployer) {
+
+module.exports = async function(deployer) {
+    //Deploy Token 
+    deployer.deploy(Token)
+    const token = await Token.deployed()
+    //Deploy EthSwap
     deployer.deploy(EthSwap)
+    const ethSwap = await EthSwap.deployed()
+
+    //Transfer all tokens to EthSwap (1 million)
+    await token.transfer(ethSwap.address, '1000000000000000000000000')
 }
