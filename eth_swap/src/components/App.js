595c3dfd7515f728256a6561a28c381112cb070f
lopeselio <lopeselio@gmail.com>
2020-06-26 22:43:48 +0530

    imported web3

--- eth_swap/src/components/App.js
+++ eth_swap/src/components/App.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+// import logo from '../logo.png';
+import Web3 from 'web3'
 import './App.css';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    console.log(window.web3)
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts[0])
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+  // Legacy dapp browsers...
+    else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider)
+        // Acccounts always exposed
+        // web3.eth.sendTransaction({/* ... */});
+    }
+    // Non-dapp browsers...
+    else {
+        window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = { items: [], text: ''}
+    // this.handleChange = this.handleChange.bind(this)
+    // this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
   render() {
     return (
       <div>
