10865850b4fe2262b4e2dc636baf3e2694790314
lopeselio <lopeselio@gmail.com>
2020-06-27 02:50:28 +0530

    app works

--- eth_swap/src/components/Navbar.js
+++ eth_swap/src/components/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
 
   render() {
     return (
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <nav className="navbar navbar-light fixed-top bg-dark flex-md-nowrap p-0 shadow">
             <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
             href="http://www.dappuniversity.com/bootcamp"
